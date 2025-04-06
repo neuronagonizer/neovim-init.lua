1661e95af33b410fcbaad35863834b9a27c30555
lamentfulcyberia <genkigirlshisamoto@tutamail.com>
2025-04-06 18:18:16 -0400

    setup zls

--- init.lua
+++ init.lua
@@ -18,6 +18,7 @@ Plug('nvim-lua/plenary.nvim')
 Plug('nvim-tree/nvim-web-devicons')
 Plug('willothy/nvim-cokeline')
 Plug('leafo/moonscript-vim')
+Plug('neovim/nvim-lspconfig')
 Plug('ziglang/zig.vim')
 Plug('pluffie/neoproj')
 Plug('ryanoasis/vim-devicons')
@@ -27,8 +28,25 @@ vim.g.tokyonight_style = "night"
 vim.cmd[[colorscheme tokyonight]]
 vim.opt.termguicolors = true
 
+-- Don't show parse errors in a separate window
+vim.g.zig_fmt_parse_errors = 0
+-- Disable format-on-save from nvim-lspconfig + ZLS
+vim.g.zig_fmt_autosave = 0
+-- Enable format-on-save from nvim-lspconfig + ZLS
+--
+-- Formatting with ZLS matches `zig fmt`
+-- For more about `zig fmt` see:
+-- https://github.com/ziglang/zig/wiki/FAQ
+vim.api.nvim_create_autocmd('BufWritePre',{
+  pattern = {"*.zig", "*.zon"},
+  callback = function(ev)
+    vim.lsp.buf.format()
+  end })
+
 require('feline').setup()
 require('cokeline').setup()
+local lspconfig = require('lspconfig')
+lspconfig.zls.setup{}
 require'lspconfig'.pyright.setup{}
 require'lspconfig'.bashls.setup{}
 require'lspconfig'.lua_ls.setup{}
